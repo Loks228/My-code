2f9408223616cf4a2b6593c41a497f1da42f58e4
Loks228 <kolya20081983@gmail.com>
2025-06-26 15:22:39 +0300

    K8s flask off)

--- Flask.py
+++ Flask.py
@@ -1,18 +1,10 @@
 from flask import Flask, render_template
-from db import SaitDB
 
 app = Flask(__name__)
-db = SaitDB()  # Создайте экземпляр вашего класса базы данных
 
 @app.route('/My_sait.html')
 def my_sait():
     return render_template('My_sait.html')
-
-@app.route('/api/add_user', methods=['POST'])
-def add_user():
-    user_id = request.form.get('user_id')  # Получите user_id из запроса
-    db.add_user(user_id)  # Добавьте пользователя в базу данных
-    return 'User added successfully', 200
 
 if __name__ == '__main__':
     app.run(debug=True)
