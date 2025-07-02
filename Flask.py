f02b20d15c998066b0d25d0f24ef2478fe20626f
Loks228 <kolya20081983@gmail.com>
2025-07-02 20:00:52 +0300

    Azure02

--- Flask.py
+++ Flask.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def my_sait():
     return render_template('My_sait.html')
 
+@app.route("/")
+def home():
+    return "Hello, World!"
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
